6343963550b31cdc8da0619d0433278f9e9ffa5f
Lalanke Athauda <lalankeba@gmail.com>
2024-06-17 10:36:45 +0530

    logging more details in error handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,10 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import logger from '../config/logger';
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    logger.error('Error occurred');
-    logger.error(err.stack);
+    logger.error(`Error occurred when accessing ${req.url}`);
 
     const statusCode = err.statusCode || 500;
+
+    if (statusCode === 404) {
+        logger.error(err.message);
+    } else {
+        logger.error(err.stack);
+    }
 
     const response = {
         message: err.message || 'Internal Server Error',
