b29c7409bdbd008b3c2be9f79db9e2623ad40262
Lalanke Athauda <lalankeba@gmail.com>
2024-06-17 10:35:48 +0530

    fixing linting issues in request logger

--- dist/middleware/request-logger.js
+++ dist/middleware/request-logger.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = __importDefault(require("../config/logger"));
 const requestLogger = (req, res, next) => {
-    let ipAddress = getIpAddress(req);
+    const ipAddress = getIpAddress(req);
     logger_1.default.info(`From: ${ipAddress} ${req.method} ${req.url}`);
     next();
 };
